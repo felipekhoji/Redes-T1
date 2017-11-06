1a30899448b9aeebc6e98907500bc32590726fef
felipekhoji <felipemyose@gmail.com>
2017-11-06 08:52:54 -0800

    testanto biblioteca sockets

---
+++ src/cgi-bin/webserver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+import cgi
+import cgitb
+
+print "Content-Type: text/html;charset=utf-8\r\n\r\n"
+
+cgitb.enable()
+
+parametros = cgi.FieldStorage()
+print parametros
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
+
+print "received data:", data
